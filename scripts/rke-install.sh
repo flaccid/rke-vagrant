93a51289a47238df5704ef8365cc14994f44863e
Chris Fordham <cfordham@scentregroup.com>
2017-12-05 18:18:42 +1100

    Remove additional echo in script.

--- scripts/rke-install.sh
+++ scripts/rke-install.sh
@@ -1,11 +1,10 @@
 #!/bin/sh -e
 
-echo 'install rke'
+echo 'provision rke'
 
 export PATH="$PATH:/opt/bin"
 
 if ! command -v rke >/dev/null; then
-  echo 'install rke'
   mkdir -p /opt/bin
   curl -LSs https://github.com/rancher/rke/releases/download/v0.0.7-dev/rke_linux-amd64 > /opt/bin/rke
   chmod +x /opt/bin/rke
