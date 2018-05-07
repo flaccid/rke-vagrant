fbe33e46f649478757eb23201a89eb54073350d8
Chris Fordham <cfordham@scentregroup.com>
2018-05-08 00:27:45 +1000

    Minor presentational enhancement for log statements.

--- scripts/setup-pki.sh
+++ scripts/setup-pki.sh
@@ -5,14 +5,14 @@ echo 'provision pki'
 ssh_user=ubuntu
 home="$(eval echo ~$ssh_user)"
 
-echo "setup pki ($ssh_user: $home)"
+echo "> setup pki ($ssh_user: $home)"
 
 curl -Ss https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub >> "$home/.ssh/authorized_keys"
 
 mkdir -p "$home/.ssh"
 
 if [ ! -e "$home/.ssh/id_rsa" ]; then
-  echo 'install insecure private ssh key'
+  echo '> install insecure private ssh key'
   curl -Ss https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant > "$home/.ssh/id_rsa"
 fi
 
