1d0896007877e3cf89d86aec0737e7017d70ccd4
Chris Fordham <cfordham@scentregroup.com>
2018-05-08 00:16:57 +1000

    Install docker 17.03 and add a small sleep for wait.

--- scripts/provision-docker.sh
+++ scripts/provision-docker.sh
@@ -3,6 +3,9 @@
 echo 'provision docker'
 
 if ! command -v docker >/dev/null; then
-  curl -Ss https://releases.rancher.com/install-docker/1.12.sh | sh
+  curl -Ss https://releases.rancher.com/install-docker/17.03.sh | sh
   usermod -aG docker ubuntu
 fi
+
+# allow a few seconds for the docker.sock to be available
+sleep 5
