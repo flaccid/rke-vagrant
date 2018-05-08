a22101f609fed0e583219d99a9e470a3e38da03c
Chris Fordham <cfordham@scentregroup.com>
2018-05-08 12:40:12 +1000

    User and perm fix for config usage.

--- scripts/rke-up.sh
+++ scripts/rke-up.sh
@@ -2,4 +2,8 @@
 
 echo 'rke up!'
 
+# ubuntu user needs access to the vagrant group to the access shared folder
+sudo usermod -a -G vagrant ubuntu
+sudo chmod 774 /usr/local/etc
+
 su ubuntu -c '/opt/bin/rke up --config /usr/local/etc/cluster.yml'
