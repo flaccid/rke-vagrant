bcb2803979b8b445da628707c4aac96b0e02d44c
Chris Fordham <cfordham@scentregroup.com>
2018-05-08 12:40:32 +1000

    Minor user feedback enhancement.

--- scripts/rke-install.sh
+++ scripts/rke-install.sh
@@ -6,6 +6,8 @@ export PATH="$PATH:/opt/bin"
 
 if ! command -v rke >/dev/null; then
   mkdir -p /opt/bin
-  curl -LSs https://github.com/rancher/rke/releases/download/v0.0.7-dev/rke_linux-amd64 > /opt/bin/rke
+  echo '> download rke'
+  curl -LSs \
+      https://github.com/rancher/rke/releases/download/v0.1.6/rke_linux-amd64 > /opt/bin/rke
   chmod +x /opt/bin/rke
 fi
