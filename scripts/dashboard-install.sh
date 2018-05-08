4354b12246da616dbbf61df62d32321550b307d3
Chris Fordham <cfordham@scentregroup.com>
2018-05-08 12:41:20 +1000

    Idempotent install of dashboard with explicity ref to kube config; setup kubectl-proxy.

--- scripts/dashboard-install.sh
+++ scripts/dashboard-install.sh
@@ -1,5 +1,19 @@
 #!/bin/sh -e
 
-echo 'install kubernetes dashboard'
+# https://kubernetes.io/docs/tasks/access-application-cluster/web-ui-dashboard/
 
-kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
+if ! kubectl --kubeconfig /usr/local/etc/kube_config_cluster.yml \
+      get pods --all-namespaces | grep dashboard; then
+  echo '> install kubernetes dashboard'
+  kubectl --kubeconfig /usr/local/etc/kube_config_cluster.yml \
+    create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
+
+  # https://github.com/kubernetes/dashboard/wiki/Access-control#admin-privileges
+  kubectl --kubeconfig /usr/local/etc/kube_config_cluster.yml \
+    create -f /usr/local/etc/dashboard-admin.yaml
+else
+  echo '> dashboard is already deployed'
+fi
+
+echo '> start kubectl proxy'
+screen -dmS kubectl-proxy bash -c 'kubectl --kubeconfig /usr/local/etc/kube_config_cluster.yml proxy --disable-filter=true --address=0.0.0.0'
