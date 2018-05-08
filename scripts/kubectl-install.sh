0d3681be83dd23158d7a511363c36cd182b24ebf
Chris Fordham <cfordham@scentregroup.com>
2018-05-08 12:40:47 +1000

    Do not install kubectl if detected.

--- scripts/kubectl-install.sh
+++ scripts/kubectl-install.sh
@@ -2,8 +2,11 @@
 
 echo 'install kubectl'
 
-# install kubectl
-sudo mkdir -p /usr/local/bin
-sudo curl -SsL \
-  "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl" -o /usr/local/bin/kubectl
-sudo chmod +x /usr/local/bin/kubectl
+if ! command -v kubectl >/dev/null; then
+  sudo mkdir -p /usr/local/bin
+  sudo curl -SsL \
+    "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl" -o /usr/local/bin/kubectl
+  sudo chmod +x /usr/local/bin/kubectl
+else
+  echo '> kubectl already installed'
+fi
